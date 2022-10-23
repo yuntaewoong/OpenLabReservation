4792af51abd836d22072ee7f3fd4aea784b57019
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2022-10-23 21:17:27 +0900

    Add Basic Page Move Feature

--- Frontend_Flutter/lib/main.dart
+++ Frontend_Flutter/lib/main.dart
@@ -1,34 +1,61 @@
-import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-void main() {//main함수, 프로그램의 시작
-  runApp(MaterialApp(home: MyApp(),));
+import 'SubPages/DateSelectionPage.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
 }
-class MyApp extends StatefulWidget{// MyApp이라는 위젯 선언
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp>{// MyApp의 State선언
-
-  Future fetch() async{//fetch함수 : get함수 호출후 결과를 비동기로 받겠다
-    var res = await http.get(Uri.parse("http://10.0.2.2:3000/"));//10.0.2.2는 가상머신 호스트의 IP를 의미
-    return json.decode(res.body);//받은 데이터를 json으로 변환
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NewButton',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MainPage(),
+    );
   }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){//State가 만들어낼 위잿 정의
-    return Scaffold(//Scaffold를 만듦
-      body: Center(//정중앙에
-        child: FutureBuilder(//비동기 함수가 리턴하는 Future값을 보여준다. 
-          future: fetch(),//fetch비동기 함수로 Future값을 가져오겠다
-          builder: (context,snap){
-            if(!snap.hasData) return const CircularProgressIndicator();//아직 데이터가 안오면 빙글빙글 아이콘이 뜸
-            return Text(snap.data['key'].toString());//데이터 오면 Text로 만들어 출력
-          }
-        ),
-      ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: (){// 버튼 클릭시 실행되는 함수
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const DateSelectionPage())//날짜 선택 페이지로
+                  );
+                },
+                child: Text('예약'),
+                //ElevatedButton 은 backgroundColor 속성이 없다.
+                //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      // shape : 버튼의 모양을 디자인 하는 기능
+                        borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    elevation: 0.0,
+                    minimumSize: Size(400,100),
+                    textStyle: const TextStyle(
+                      fontSize : 80.0
+                    )
+                ),
+              )
+            ],
+          ),
+        )
     );
   }
 }
