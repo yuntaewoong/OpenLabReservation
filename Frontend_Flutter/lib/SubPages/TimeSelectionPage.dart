5965b04b4aa15e55b5d563d0bf285438eb69ccee
SPark <ppku@hanmail.net>
2022-11-17 17:22:51 +0900

    시간 선택 페이지에 색깔 기능 정하는 colors 함수 추가 예약 페이지 수정해서 뒤로가기 버튼 문제 해결

--- Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
+++ Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
@@ -3,27 +3,34 @@ import 'package:flutter/material.dart';
 import 'ReservationPage.dart';
 import 'InformationPage.dart';
 import 'DateSelectionPage.dart';
+bool reservation = false;
 
 class TimeSelectionPage extends StatelessWidget {
   const TimeSelectionPage({Key? key}) : super(key: key);
+
+  Color colors() { //예약 상황에 따라 다른 색갈로 버튼을 표시
+    Color col;
+    if(reservation) {col = Colors.red;}
+    else {col = Colors.green;}
+    return col;
+  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-
-
       appBar: AppBar(
-        title: Text("선택한 날짜 여기에 표시"),
+        title: const Text("선택한 날짜 여기에 표시"),
+        actions: <Widget>[]
       ),
       body: ListView(
       scrollDirection : Axis.vertical,
       children : <Widget> [
         for (num i=8; i < 10; i++)
           ListTile(
-            title: Text("0$i:00 ~ 0$i:59"),
-            tileColor: Colors.red,
-            trailing: Icon(Icons.navigate_next),
+            title: Text("0$i:00 ~ 0$i:59", style: const TextStyle(fontSize: 30,) ),
+            tileColor: colors(),
+            trailing: Icon(Icons.navigate_next, size: 40,),
             onTap: () {
               Navigator.push(
                   context,
@@ -34,9 +41,9 @@ class TimeSelectionPage extends StatelessWidget {
           ),
         for (num i=10; i < 24; i++)
           ListTile(
-              title: Text("$i:00 ~ $i:59"),
-              tileColor: Colors.green,
-              trailing: Icon(Icons.navigate_next),
+              title: Text("$i:00 ~ $i:59", style: const TextStyle(fontSize: 30,)),
+              tileColor: colors(),
+              trailing: Icon(Icons.navigate_next, size: 40,),
               onTap: () {
                 Navigator.push(
                   context,
