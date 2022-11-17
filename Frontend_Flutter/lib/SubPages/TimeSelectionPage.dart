876732890f80f002cd562bd2bad987ebf678b259
SPark <ppku@hanmail.net>
2022-11-17 10:07:13 +0900

    time scroll 기능 추가

--- Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
+++ Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
@@ -9,7 +9,47 @@ class TimeSelectionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
+
+      appBar: AppBar(
+        title: Text("시간 선택"),
+      ),
+      body: ListView(
+      scrollDirection : Axis.vertical,
+      children : <Widget> [
+        for (num i=8; i < 10; i++)
+          ListTile(
+            title: Text("0$i:00 ~ 0$i:59"),
+            tileColor: Colors.red,
+            trailing: Icon(Icons.navigate_next),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ReservationPage())
+              ); // MaterialPageRoute
+            }, // onTap
+          ),
+        for (num i=10; i < 24; i++)
+          ListTile(
+              title: Text("$i:00 ~ $i:59"),
+              tileColor: Colors.green,
+              trailing: Icon(Icons.navigate_next),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ReservationPage())
+                ); // MaterialPageRoute
+              }, // onTP
+          ),
+    ]
+    )
+    );
+
+
+    /*return Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -86,6 +126,6 @@ class TimeSelectionPage extends StatelessWidget {
             ],
           ),
         )
-    );
+    ); */
   }
 }
