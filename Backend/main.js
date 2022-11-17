6baff97569bcfa2416b5f4bdea893085119118f1
yoon <15ywt@naver.com>
2022-11-17 10:42:16 +0900

    Nodejs Sqlite3 연동 테스트

--- Backend/main.js
+++ Backend/main.js
@@ -1,6 +1,23 @@
 
 var express = require("express");
 var app = express();
+var sqlite3 = require('sqlite3').verbose();
+
+var db = new sqlite3.Database('./DataBase/Reservation.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the Reservation Database.');
+})
+
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
+
+
 
 app.get("/",(req,res) =>{
     res.json({
