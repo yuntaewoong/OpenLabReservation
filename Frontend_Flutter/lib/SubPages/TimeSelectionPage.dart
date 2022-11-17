b35eaf041f19ab2c0a7ca45b06a1e175f82d7f23
SPark <ppku@hanmail.net>
2022-11-17 20:43:24 +0900

    시간 선택 페이지 수정; 예약상태 리스트 받아 시간대별 예약 상태에 따라 색 다르게 표시하고, 예약 가능한 시간대에만 예약 페이지로 이동하도록 구현

--- Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
+++ Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 import 'ReservationPage.dart';
 import 'InformationPage.dart';
 import 'DateSelectionPage.dart';
-bool reservation = false;
+var reservation = [true, true, true, false, false, false, false, false, true,
+  true, true, false, false, false, false, false];
 
 class TimeSelectionPage extends StatelessWidget {
   const TimeSelectionPage({Key? key}) : super(key: key);
 
-  Color colors() { //예약 상황에 따라 다른 색갈로 버튼을 표시
+  Color colors(int i) { //예약 상황에 따라 다른 색갈로 버튼을 표시
     Color col;
-    if(reservation) {col = Colors.red;}
+    if(reservation[i]) {col = Colors.red;}
     else {col = Colors.green;}
     return col;
   }
@@ -26,30 +27,38 @@ class TimeSelectionPage extends StatelessWidget {
       body: ListView(
       scrollDirection : Axis.vertical,
       children : <Widget> [
-        for (num i=8; i < 10; i++)
+        for (int i=8; i < 10; i++)
           ListTile(
-            title: Text("0$i:00 ~ 0$i:59", style: const TextStyle(fontSize: 30,) ),
-            tileColor: colors(),
-            trailing: Icon(Icons.navigate_next, size: 40,),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ReservationPage())
-              ); // MaterialPageRoute
-            }, // onTap
-          ),
-        for (num i=10; i < 24; i++)
-          ListTile(
-              title: Text("$i:00 ~ $i:59", style: const TextStyle(fontSize: 30,)),
-              tileColor: colors(),
+              title: Text(
+                  "0$i:00 ~ 0$i:59", style: const TextStyle(fontSize: 30,)),
+              tileColor: colors(i - 8),
               trailing: Icon(Icons.navigate_next, size: 40,),
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ReservationPage())
-                ); // MaterialPageRoute
-              }, // onTP
+                if (!reservation[i - 8]) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ReservationPage())
+                  ); // MaterialPageRoute
+                }
+                else;
+              } // onTap
+          ),
+
+        for (int i=10; i < 24; i++)
+          ListTile(
+              title: Text("$i:00 ~ $i:59", style: const TextStyle(fontSize: 30,)),
+              tileColor: colors(i-8),
+              trailing: Icon(Icons.navigate_next, size: 40,),
+              onTap: () {
+                if (!reservation[i - 8]) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ReservationPage())
+                  ); // MaterialPageRoute
+                }
+              } // onTaP
           ),
     ]
     )
