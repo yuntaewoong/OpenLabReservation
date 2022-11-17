58b447229009a74b7225b32839469fd82cb3638c
SPark <ppku@hanmail.net>
2022-11-17 10:17:18 +0900

    timeselectionpage에서 불필요한 주석 지움

--- Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
+++ Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
@@ -14,7 +14,7 @@ class TimeSelectionPage extends StatelessWidget {
 
 
       appBar: AppBar(
-        title: Text("시간 선택"),
+        title: Text("선택한 날짜 여기에 표시"),
       ),
       body: ListView(
       scrollDirection : Axis.vertical,
@@ -47,85 +47,5 @@ class TimeSelectionPage extends StatelessWidget {
     ]
     )
     );
-
-
-    /*return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: (){// 버튼 클릭시 실행되는 함수
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const ReservationPage())//날짜 선택 페이지로
-                  );
-                },
-                child: Text('비어 있는 시간'),
-                //ElevatedButton 은 backgroundColor 속성이 없다.
-                //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.green,
-                    shape: RoundedRectangleBorder(
-                      // shape : 버튼의 모양을 디자인 하는 기능
-                        borderRadius: BorderRadius.circular(10.0)
-                    ),
-                    elevation: 0.0,
-                    minimumSize: Size(400,100),
-                    textStyle: const TextStyle(
-                        fontSize : 80.0
-                    )
-                ),
-              ),
-              ElevatedButton(
-                onPressed: (){// 버튼 클릭시 실행되는 함수
-                  Navigator.push(
-                      context,
-                     MaterialPageRoute(builder: (context) => const InformationPage())//날짜 선택 페이지로
-                  );
-                },
-                child: Text('이미 예약된 시간'),
-                //ElevatedButton 은 backgroundColor 속성이 없다.
-                //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      // shape : 버튼의 모양을 디자인 하는 기능
-                        borderRadius: BorderRadius.circular(10.0)
-                    ),
-                    elevation: 0.0,
-                    minimumSize: Size(400,100),
-                    textStyle: const TextStyle(
-                        fontSize : 80.0
-                    )
-                ),
-              ),
-              ElevatedButton(
-                onPressed: (){// 버튼 클릭시 실행되는 함수
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const DateSelectionPage())//날짜 선택 페이지로
-                  );
-                },
-                child: Text('날짜 선택으로'),
-                //ElevatedButton 은 backgroundColor 속성이 없다.
-                //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.pinkAccent,
-                    shape: RoundedRectangleBorder(
-                      // shape : 버튼의 모양을 디자인 하는 기능
-                        borderRadius: BorderRadius.circular(10.0)
-                    ),
-                    elevation: 0.0,
-                    minimumSize: Size(400,100),
-                    textStyle: const TextStyle(
-                        fontSize : 80.0
-                    )
-                ),
-              )
-            ],
-          ),
-        )
-    ); */
   }
 }
