d9b1215cad65083ada28bb454d82cbb56c58bfa2
yoon <15ywt@naver.com>
2022-12-02 14:59:32 +0900

    달력페이지에 달력만 남기고, 달력에서 선택한 날짜로 프로그램 동작하게 변경

--- Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
+++ Frontend_Flutter/lib/SubPages/TimeSelectionPage.dart
@@ -10,7 +10,10 @@ import 'package:http/http.dart' as http;
 
 
 class TimeSelectionPage extends StatelessWidget {
-  const TimeSelectionPage({Key? key}) : super(key: key);
+  TimeSelectionPage(this.month,this.date);
+  int month;
+  int date;
+
   Future FetchReservation(int month,int date,int time) async{//fetch함수 : get함수 호출후 결과를 비동기로 받겠다
     var res = await http.get(Uri.parse(
         "http://10.0.2.2:3000/" +
@@ -33,7 +36,7 @@ class TimeSelectionPage extends StatelessWidget {
       children : <Widget> [
         for (int i=8; i < 10; i++)
           FutureBuilder(
-            future: FetchReservation(7,3,i),//7월3일정보, 이 데이터는 다른 페이지에서 받아올것
+            future: FetchReservation(this.month,this.date,i),
             builder: (context,snap){
               if(!snap.hasData) return const CircularProgressIndicator();//아직 데이터가 안오면 빙글빙글 아이콘이 뜸
               return ListTile(
@@ -46,7 +49,7 @@ class TimeSelectionPage extends StatelessWidget {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => ReservationPage(7,3,i)
+                              builder: (context) => ReservationPage(this.month,this.date,i)
                           )
                       ); // MaterialPageRoute
                     }
@@ -54,7 +57,7 @@ class TimeSelectionPage extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => CancelPage(7,3,i)
+                          builder: (context) => CancelPage(this.month,this.date,i)
                         )
                       );
                     }
@@ -65,7 +68,7 @@ class TimeSelectionPage extends StatelessWidget {
 
         for (int i=10; i < 24; i++)
           FutureBuilder(
-              future: FetchReservation(7,3,i),//7월3일정보, 이 데이터는 다른 페이지에서 받아올것
+              future: FetchReservation(this.month,this.date,i),
               builder: (context,snap){
                 if(!snap.hasData) return const CircularProgressIndicator();//아직 데이터가 안오면 빙글빙글 아이콘이 뜸
                 return ListTile(
@@ -77,13 +80,13 @@ class TimeSelectionPage extends StatelessWidget {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => ReservationPage(7,3,i))
+                                builder: (context) => ReservationPage(this.month,this.date,i))
                         ); // MaterialPageRoute
                       }
                       else {
                         Navigator.push(
                             context,
-                          MaterialPageRoute(builder: (context) => CancelPage(7,3,i))
+                          MaterialPageRoute(builder: (context) => CancelPage(this.month,this.date,i))
                         );
                       };
                     } // onTaP
